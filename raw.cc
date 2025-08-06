3f713bea85e3244887436779c40e00109b5b16a6
David Sheffield <sheffield.david@gmail.com>
2025-08-05 20:20:19 -0700

    add license to code and headers

--- raw.cc
+++ raw.cc
@@ -1,3 +1,32 @@
+// BSD 3-Clause License
+
+// Copyright (c) 2025, dsheffie
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/mman.h>
@@ -64,14 +93,16 @@ void load_raw(const char* fn, state_t *ms) {
   }
   munmap(buf, s.st_size);
   close(fd);
-
-  printf("firmware starts %lx, dram starts at %lx, os kernel at %lx\n",
-	 globals::fw_start_addr, globals::ram_phys_start, kern_addr);
-  
+  std::cout << "firmware starts " << std::hex <<globals::fw_start_addr
+	    << ", dram starts at " << globals::ram_phys_start
+	    << ", kernel at " << kern_addr
+	    << std::dec << "\n";
+    
   
 
   if(not(initrd_addr == 0 and initrd_size == 0)) {
-    printf("initrd_addr %lx, initrd_size %lx\n", initrd_addr, initrd_size);
+    std::cout << "initrd_addr " << std::hex << initrd_addr << std::hex
+	      << ", initrd_size " << initrd_size << "\n";
   }
   
   riscv_build_fdt(&mem[globals::fdt_addr],
